8b8078934330b729715595485fd3c6f34d9ebff4
Pabllo Cristian <pabllo.dev@gmail.com>
2024-08-19 18:18:08 -0300

    Initial commit

---
+++ js/companies.js
@@ -0,0 +1,19 @@
+const companies = [
+    {
+        name: 'Amazon',
+        founded: '1994',
+        industry: ['E-Commerce', 'Cloud'],
+    },
+    {
+        name: 'Facebook',
+        founded: '2004',
+        industry: ['Social'],
+    },
+    {
+        name: 'Alphabet Inc.',
+        founded: '2015',
+        industry: ['Search', 'Cloud', 'Advertising'],
+    }
+];
+
+module.exports = companies;
---
+++ js/showObj.js
@@ -0,0 +1,21 @@
+const companies = require('./companies');
+
+//Adding kind
+companies.forEach((company) => {
+    company.kind = 'Internet company'
+});
+
+function dots(name) {
+    const maxChar = 20;
+    const dotsQuant = maxChar - name.length;
+    return '.'.repeat(dotsQuant);
+}
+
+//Showing the companies names and theirs years of foundation
+function show(companies) {
+    return companies.name + dots(companies.name) + companies.founded;
+}
+
+companies.forEach((company) => {
+    console.log(show(company));
+})
